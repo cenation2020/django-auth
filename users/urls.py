e90b62b59ab826ed74f2fd0b392d9bf0e29e0b1d
cenation2020 <96724040+cenation2020@users.noreply.github.com>
2022-04-01 19:24:59 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView
+from .views import SubscribeView, RegisterView, LoginView, UserView, LogoutView, HardRegisterView
 
 urlpatterns = [
+    path('subscribe', SubscribeView.as_view()),
     path('register', RegisterView.as_view()),
+    path('hard-register', HardRegisterView.as_view()),
     path('login', LoginView.as_view()),
     path('user', UserView.as_view()),
     path('logout', LogoutView.as_view()),
